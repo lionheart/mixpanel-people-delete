7e6eedada711d53e9c31def95ab1a40b4974e7ca
Dan Loewenherz <dan@lionheartsw.com>
2018-05-29 12:54:58 -0500

    add requirements file

--- main.py
+++ main.py
@@ -10,6 +10,4 @@ for i in range(num_projects):
     MIXPANEL_TOKEN = os.environ.get("MIXPANEL_TOKEN_" + str(i))
 
     mixpanel = Mixpanel(MIXPANEL_SECRET, token=MIXPANEL_TOKEN)
-
-    # mixpanel.export_people("pushpin_people_export.txt", "", timezone_offset=-8)
     mixpanel.people_delete(query_params={})
